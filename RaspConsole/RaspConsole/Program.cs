84ce055608a76792b2afc03fffde6e6d274e84c6
jarlelin <jarle.lindseth@gmail.com>
2015-03-17 23:01:40 +0100

    Første commit. Enkel console app.

---
+++ RaspConsole/RaspConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaspConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Console started on " + Environment.OSVersion.Platform.ToString()));
+
+            Console.ReadLine();
+
+            Console.WriteLine("Exiting...");
+        }
+    }
+}
